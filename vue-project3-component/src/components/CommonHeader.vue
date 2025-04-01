cae420a87931e4fe8fa22aa25239aadb70039950
hyungEee <jihyung772@gmail.com>
2025-04-01 17:42:54 +0900

    ui/ux 특강 2일차

---
+++ vue-project3-component/src/components/CommonHeader.vue
@@ -0,0 +1,6 @@
+<template>
+  <header>
+    <h1>💗 UI Component Gallery 💗</h1>
+    <p>css로 만든 다양한 기본 컴포넌트를 한눈에 확인하세요!</p>
+  </header>
+</template>
---
+++ vue-project3-component/src/components/MainGallery.vue
@@ -0,0 +1,28 @@
+<script setup>
+import BaseButton from './UI/BaseButton.vue'
+import BaseAlert from './UI/BaseAlert.vue'
+import BaseBadge from './UI/BaseBadge.vue'
+</script>
+<template>
+  <!-- grid container -->
+  <main class="component-grid">
+    <!-- component group (grid item) -->
+    <section class="component-card">
+      <h2>버튼</h2>
+      <BaseButton variant="primary">Primary</BaseButton>
+      <BaseButton variant="outline">Outline</BaseButton>
+      <BaseButton variant="pill">Pill</BaseButton>
+    </section>
+    <section class="component-card">
+      <h2>알림창</h2>
+      <BaseAlert variant="success">success message</BaseAlert>
+      <BaseAlert variant="warning">warning message</BaseAlert>
+      <BaseAlert variant="error">error message</BaseAlert>
+    </section>
+    <section class="component-card">
+      <h2>배지</h2>
+      <BaseBadge variant="success">Success</BaseBadge>
+      <BaseBadge variant="number">3</BaseBadge>
+    </section>
+  </main>
+</template>
